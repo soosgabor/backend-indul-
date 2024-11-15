777e69c7986a261641ca488552a16b4ce60c98e7
Soós Gábor <soos.gabor@jedlik.eu>
2024-11-15 11:29:52 +0100

    A képzésekhez tartozó végpontok JSON választ adnak

--- routes/routes.js
+++ routes/routes.js
@@ -4,47 +4,24 @@ const oneSideModel = require("../models/model_1");
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  const data = new nSideModel({
-    name: req.body.name,
-    foreignKey: req.body.foreignKey,
-  });
+router.get('/trainings', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all trainings ' })
+})
 
-  try {
-    res.send('POST')
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+router.get('/trainings/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Get training ${req.params.id}` })
+})
 
-router.get("/", async (req, res) => {
-  try {
-    res.send('GET all')
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.post('/trainings', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new training' })
+})
 
-router.get("/:id", async (req, res) => {
-  try {
-    res.send('GET')
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.put('/trainings/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Update training ${req.params.id}` })
+})
 
-//Update by ID Method
-router.patch("/:id", async (req, res) => {
-  try {
-    res.send('PATCH')
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-//Delete by ID Method
-router.delete("/:id", async (req, res) => {
-  res.send('DELETE')
-});
+router.delete('/trainings/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Delete training ${req.params.id}` })
+})
 
 module.exports = router;
