5b60fd2ba3cefa6bfb1a5cc12f3f579073abfbf4
Soós Gábor <soos.gabor@jedlik.eu>
2024-11-15 11:23:26 +0100

    Start

--- models/model_n.js
+++ models/model_n.js
@@ -5,34 +5,7 @@ function customValidator(num) {
 }
 
 const nSideSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
-    unique: true,
-    trim: true,
-    lowercase: true,
-    maxlength: [50, "Name can not be more than 50 characters!"],
-  },
-  photo: {
-    type: String,
-    default: "no-photo.jpg",
-  },
-  createdAt: {
-    type: Date,
-    max: [Date.now, "createAt can not be greater than actual date"],
-  },
-  number: {
-    type: Number,
-    validate: [
-      customValidator,
-      "A number-nek 1000-rel oszthatónak kell lennie!",
-    ],
-  },
-  foreignKey: {
-    type: Number,
-    required: true,
-    ref: "oneSideModel",
-  },
+  
 });
 
 module.exports = mongoose.model("nSideModel", nSideSchema, "nCollectionName"); // ezzel a névvel lesz létrehozva a kollekció.
